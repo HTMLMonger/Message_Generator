73567176cdf4a692643ca5ad7432d59c7314e01a
M Mack <leithpcman@gmail.com>
2024-02-02 12:28:42 +0000

    removed unnecessary html and css files,and added README.md

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 'use strict'
-
+//define words object that store all words
 let words = {
-    adjectives:[ 'adorable', 'attractive', 'beautiful', 'cruel', 'clever', 'tasty','attractive','bald','beautiful','chubby','clean','dazzling','drab','elegant','fancy','flabby','glamorous','gorgeous','handsome','tall','magnificent','muscular','plain','plump','quaint','scruffy','shapely','short','skinny','stocky','ugly','unkempt','unsightly' ],
-    nouns:['cat', 'dog', 'hamster', 'bird', 'rat', 'snake', 'spider', 'horse', 'guinea pig','teacher','doctor','chef','student','pilot','firefighter','lawyer','mechanic','engineer','farmer'] ,
+    adjectives:[ 'adorable', 'attractive', 'beautiful', 'cruel', 'clever', 'tasty','attractive','bald','beautiful','chubby','clean','dazzling','drab','elegant','fancy','flabby','glamorous','gorgeous','handsome','tall','magnificent','muscular','plain','plump','quaint','scruffy','shapely','short','skinny','ugly','unkept','unsightly' ],
+    nouns:['cat', 'dog', 'hamster', 'bird', 'rat', 'snake', 'spider', 'horse', 'guinea pig','teacher','doctor','chef','student','pilot','firefighter',
+    'lawyer','mechanic','engineer','farmer'] ,
     verbs:['always','usually','often','sometimes','rarely','never','hardly ever','occasionally','seldom','generally','frequently','normally','once','twice']
 }
-let firstWord = words.adjectives[(Math.floor(Math.random()* words.adjectives.length-1))];
-let secondWord = words.nouns[(Math.floor(Math.random()* words.nouns.length-1))];
-let thirdWord = words.verbs[(Math.floor(Math.random()* words.verbs.length-1))];
-let fourthWord = words.adjectives[(Math.floor(Math.random()* words.adjectives.length-1))];
 
+const generateRandoNum = num => {
+    return Math.floor(Math.random() * num);
+  }
 
-console.log(`You are ${firstWord}, and there is also a ${secondWord} who thinks he is ${thirdWord} ${fourthWord} `)
+//define variables to hold random words for sentence
+let firstWord = words.adjectives[generateRandoNum(words.adjectives.length)];
+let secondWord = words.nouns[generateRandoNum(words.nouns.length)];
+let thirdWord = words.verbs[generateRandoNum(words.verbs.length)];
+let fourthWord = words.adjectives[generateRandoNum(words.adjectives.length)];
+
+//log sentence including random words
+console.log(`You are ${firstWord}, but there is a ${secondWord} nearby who thinks he is ${thirdWord} ${fourthWord}.👍`)
